6f2cb06f77970185d381dda3c12eaea9ddcd67fb
Arnau Valbuena Torrellas <arnau.valbuena@students.salle.url.edu>
2020-04-26 18:28:29 +0200

    Commit DAO

--- SmartPiano_shared/src/Network/NetworkConfiguration.java
+++ SmartPiano_shared/src/Network/NetworkConfiguration.java
@@ -3,7 +3,6 @@ package Network;
 public class NetworkConfiguration {
     // constants relacionades amb la comunicacio
     public static final String SERVER_IP = "127.0.0.1";
-    public static final int SERVER_PORT = 40000;
+    public static final int SERVER_PORT = 2222;
 }
 
-//aixo es nou
