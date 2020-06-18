f51282808921cab5d9878355673f8a45bf321b64
martaesna <marta.estragues@students.salle.url.edu>
2020-06-18 14:36:27 +0200

    Merge branch 'master' of C:\Users\marta\Desktop\SmartPiano with conflicts.

--- SmartPiano_server/src/Controller/MainViewController.java
+++ SmartPiano_server/src/Controller/MainViewController.java
@@ -4,30 +4,32 @@ import View.MainView;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainViewController implements ActionListener   {
+public class MainViewController implements ActionListener {
     private MainView view;
-
-
     private MainView v;
 
     public MainViewController(MainView v) {
         this.v = v;
-
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals(v.ACTION)) {
-            System.out.println("vols gestionar cansçons");
+        if (e.getActionCommand().equals(v.ACTION)) { //cuando apretamos el boton
+            System.out.println("has apretat gestiona cançons");
+            gc.setVisible(true);
+            v.setVisible(false);
         }
-        if (e.getActionCommand().equals(v.ACTION2)) {
-            System.out.println("Vols mostrar el Top 5");
+        if (e.getActionCommand().equals(v.ACTION2)) { //cuando apretamos el boton
+            System.out.println("has apretat mostra top 5");
+            mt.setVisible(true);
+            v.setVisible(false);
         }
-        if (e.getActionCommand().equals(v.ACTION3)) {
-            System.out.println("Vols mostrar evolucio");
+        if (e.getActionCommand().equals(v.ACTION3)) { //cuando apretamos el boton
+            System.out.println("has apretat mostra evolucio");
+            me.setVisible(true);
+            v.setVisible(false);
         }
-
     }
 }
 
